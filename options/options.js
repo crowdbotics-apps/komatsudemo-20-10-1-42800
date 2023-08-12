ca48bc3822b476346fbc7c2d0f3df73756c7de28
Andre Machado <andre.machado@crowdbotics.com>
2023-08-11 20:44:10 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "komatsudemo-20-10-1-42800",
-  url: "https://komatsudemo-20-10-1-42800.botics.co",
-  api: "https://komatsudemo-20-10-1-42800.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "komatsudemo-20-10-1-42800",
+  "url": "https://komatsudemo-20-10-1-42800.botics.co",
+  "api": "https://komatsudemo-20-10-1-42800.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
+  const {
+    entities: Logins
+  } = useSelector(state => state.Logins);
   return <SafeAreaView style={styles.container}>
       <View style={styles.logoContainer}>
-        <Text style={styles.logoText}>Komatsu</Text>
+        <Text style={styles.logoText}>MyKomatsu</Text>
       </View>
       <View style={styles.formContainer}>
         <TextInput style={styles.input} placeholder="Username" placeholderTextColor="#999" />
-        <TextInput style={styles.input} placeholder="Password" placeholderTextColor="#999" secureTextEntry />
-        <TouchableOpacity style={styles.loginButton}>
+        <TextInput style={[styles.input, styles.poXQjliz]} placeholder="Password" placeholderTextColor="#999" secureTextEntry />
+        <TouchableOpacity style={[styles.loginButton, styles.JhvGdGcI]} data={Logins} onPress={() => {
+        navigation.navigate("welcome");
+      }}>
           <Text style={styles.loginButtonText}>Login</Text>
         </TouchableOpacity>
       </View>
@@ -19,25 +27,25 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
   },
   logoContainer: {
     marginBottom: 50
   },
   logoText: {
     fontSize: 32,
-    fontWeight: 'bold',
-    color: '#007AFF'
+    fontWeight: "bold",
+    color: "#007AFF"
   },
   formContainer: {
-    width: '80%'
+    width: "80%"
   },
   input: {
     height: 50,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 20,
@@ -45,15 +53,21 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     height: 50,
-    backgroundColor: '#007AFF',
+    backgroundColor: "#007AFF",
     borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   loginButtonText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#fff'
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  poXQjliz: {
+    paddingTop: 5
+  },
+  JhvGdGcI: {
+    marginTop: 10
   }
 });
 export default LoginScreen;
